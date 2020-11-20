14e8789c863d48d97287cf734f96863e1161629a
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-11-19 23:06:16 -0700

    added entries

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
+import { getEntries } from '../redux/actions/EntriesAction'
 
 function Dashboard(props) {
-	console.log(props.user)
+	useEffect(() => {
+		props.getEntries(props.user?.user.id)
+	}, [props.user.loaded])
+
 	return (
 		<div className='dash-container'>
 			<h2 className='dash-title'>Dashboard</h2>
@@ -10,6 +14,17 @@ function Dashboard(props) {
 				<p className='loading'>Loading...</p>:
 				<div className='dash-content'>
 					<p className='dash-user'>Welcome to the dashboard, {props.user.user.first_name}</p>
+					<div className='entries-container'>
+						{props.entries.entries.map(entry => (
+							<div className='single-entry-container'>
+								<h4>Name: {entry.name}</h4>
+								<p>Food: {entry.food_name}</p>
+								<p>Category: {entry.category}</p>
+								<p>Amount: {entry.amount}</p>
+								<p>Date: {entry.date}</p>
+							</div>
+						))}
+					</div>
 				</div>
 			}
 		</div>
@@ -19,8 +34,12 @@ function Dashboard(props) {
 const mapStateToProps = state => {
 	return {
 		user: state.user,
+		entries: state.entries.entries
 	}
 }
 
+const mapDispatchToProps = {
+	getEntries
+}
 
-export default connect(mapStateToProps, null)(Dashboard)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
