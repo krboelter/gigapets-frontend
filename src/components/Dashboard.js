c449d7405a529d377bdd41fd046b341598590050
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-12-10 22:41:00 -0700

    updateing styles

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,14 +6,20 @@ import { makeStyles, styled } from '@material-ui/core/styles'
 const useStyles = makeStyles({
 	entryContainer: {
 		display: 'flex',
+		padding: '8px 12px',
 		flexDirection: 'column',
 		boxShadow: '0 0 10px black',
-		margin: '20px 10px'
+		margin: '20px 10px',
+		width: '40%',
+		backgroundColor: 'lightgray'
 	},
 	name: {
 		fontWeight: 'bold',
-		fontSize: '18px',
+		fontSize: '20px',
 		color: '#6d3fc1'
+	},
+	entryP: {
+		fontSize: '16px',
 	}
 })
 
@@ -24,7 +30,7 @@ function Dashboard(props) {
 		if (props.user?.loaded) {
 			props.getEntries(props.user?.user.id)
 		}
-	}, [])
+	}, [props.user.loaded])
 
 	return (
 		<div className='dash-container'>
@@ -33,16 +39,15 @@ function Dashboard(props) {
 				<p className='loading'>Loading...</p>:
 
 				<div className='dash-content'>
-					<p className='dash-user'>Welcome to the dashboard, {props.user.user.first_name}</p>
+					<p className='dash-user'>Welcome, {props.user.user.first_name}. Below are your most recent entries:</p>
 					<div className='entries-container'>
 						{props.entries.entries.map((entry, index) => (
 							<div key={index} className={styled.entryContainer}>
 								<h4 className={styled.name}>Name: {entry.name}</h4>
-								<p>Food: {entry.food_name}</p>
-								<p>Category: {entry.category}</p>
-								<p>Amount: {entry.amount}</p>
-								<p>Oz: {entry.amount_type /*dropdown*/}</p>
-								<p>Date: {entry.date}</p>
+								<p className={styled.entryP}>Food: {entry.food_name}</p>
+								<p className={styled.entryP}>Category: {entry.category}</p>
+								<p className={styled.entryP}>Amount: {entry.amount} {entry.amount_type}</p>
+								<p className={styled.entryP}>Entered: {entry.date}</p>
 							</div>
 						))}
 					</div>
